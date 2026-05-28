2d3ccecd989b9052ea5ddd19c14e4986663bb1f9
SavagenovaX <savagenovax@gmail.com>
2026-05-28 00:57:33 +0000

    Create config.example.h

---
+++ config.example.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+using namespace std;
+
+// Copy this file to config.h
+// Fill in your token and chat ID
+const string TOKEN   = "YOUR_BOT_TOKEN_HERE";
+const string CHAT_ID = "YOUR_CHAT_ID_HERE";
